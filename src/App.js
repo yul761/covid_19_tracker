de379209f9ac78fe6018f1df5ce9d643a0061fde
Yuchen Lin <kloselyc@gmail.com>
2020-03-15 21:58:26 -0700

    initialize api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
 function App() {
+  const Alldata = () => {
+    axios({
+      method: "GET",
+      url: "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "a274e47037msh0c88d9251a3d8ccp18bab2jsn57b96f87795b"
+      }
+      // params: {
+      //   country: "Canada"
+      // }
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  const filterByCountry = country => {
+    axios({
+      method: "GET",
+      url: "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+        "x-rapidapi-key": "a274e47037msh0c88d9251a3d8ccp18bab2jsn57b96f87795b"
+      },
+      params: {
+        country: `$ {country}`
+      }
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="country">
+        <div className="country__name">
+          Country :
+          <input
+            className="country__name-input"
+            type="text"
+            placeholder="Please Enter the country you want to search"
+            name="country"
+          ></input>
+        </div>
+      </div>
     </div>
   );
 }
