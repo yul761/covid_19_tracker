6f512dcfcef51de754fa250de6e646e9caaab551
Yuchen Lin <kloselyc@gmail.com>
2020-03-25 21:19:03 -0600

    Updates

---
+++ precache-manifest.c537a700f65e0c86c8180ac958dd8823.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b188df27c3ad33858846ef428b21b7d4",
+    "url": "/covid_19_tracker/index.html"
+  },
+  {
+    "revision": "79ad99bcc4f3428eafe0",
+    "url": "/covid_19_tracker/static/css/main.e67dfb2f.chunk.css"
+  },
+  {
+    "revision": "41b619d886e38af48478",
+    "url": "/covid_19_tracker/static/js/2.fc5cd594.chunk.js"
+  },
+  {
+    "revision": "45c6da32894df739719b81b142cbcd00",
+    "url": "/covid_19_tracker/static/js/2.fc5cd594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ad99bcc4f3428eafe0",
+    "url": "/covid_19_tracker/static/js/main.71d183c1.chunk.js"
+  },
+  {
+    "revision": "1c9d557edccaa7cb16fc",
+    "url": "/covid_19_tracker/static/js/runtime-main.a5c5ec70.js"
+  },
+  {
+    "revision": "533826bf009ec9bb886bffef1950797b",
+    "url": "/covid_19_tracker/static/media/Earth.533826bf.png"
+  },
+  {
+    "revision": "3609b6576d22a618285296d37ff69261",
+    "url": "/covid_19_tracker/static/media/background.3609b657.jpg"
+  },
+  {
+    "revision": "6a4b3b9fc5526b5ee6f7ed0f49c336ca",
+    "url": "/covid_19_tracker/static/media/specific.6a4b3b9f.png"
+  }
+]);
